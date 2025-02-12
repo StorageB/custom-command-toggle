c6f47c7089faabbdc3e72bf0112b6a01e43236ab
StorageB <ezzy25@hotmail.com>
2025-02-11 20:52:59 -0600

    update about.js

--- about.js
+++ about.js
@@ -3,14 +3,16 @@
 export const releaseNotes = `
 What's new in this version:
 
-•  Option to use keyboard shortcuts to activate toggle buttons
-•  Option to immediately hide quick settings menu after button press
+•  Option to check a command's output to determine toggle state at startup.
+•  Option to toggle button state only if command executes successfully (returns exit code 0).
+•  Fixed a compatibility issue with some systems (such as NixOS) by switching from /bin/bash to /usr/bin/env bash for command execution.
+•  Added feature to export current toggle button settings to a file.
+•  Added support for GNOME 48.
+•  Minor UI improvements.
 
 Previous version:
 
-•  Added Toggle Behavior section to button preferences
-•  Option to hide top bar indicator icon
-•  Option for an always-on or always-off toggle button
-•  Support added for GNOME 47
+•  Option to use keyboard shortcuts to activate toggle buttons.
+•  Option to immediately hide quick settings menu after button press.
 `;
 
