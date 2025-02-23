3b15eb4e4913c3077122ffcf2c074cc2f4172a1f
StorageB <ezzy25@hotmail.com>
2025-02-22 20:57:53 -0600

    update about.js

--- about.js
+++ about.js
@@ -3,12 +3,13 @@
 export const releaseNotes = `
 What's new in this version:
 
-•  Option to check a command's output to determine toggle state at startup.
+•  Option to check a command's output to determine button state at startup.
 •  Option to toggle button state only if command executes successfully (returns exit code 0).
+•  Added feature to export current button configurations to a file.
 •  Fixed a compatibility issue with some systems (such as NixOS) by switching from /bin/bash to /usr/bin/env bash for command execution.
-•  Added feature to export current toggle button settings to a file.
 •  Added support for GNOME 48.
 •  Minor UI improvements.
+•  Improved code readability.
 
 Previous version:
 
